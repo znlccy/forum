7aeecbc67274cad5f902b040ff23405c6c8279e2
znlccy <756311868@qq.com>
2018-08-07 15:25:09 +0800

    Commit Framework File

---
+++ application/index/controller/BaseController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/7
+ * Time: 15:09
+ * Comment: 基础控制器
+ */
+namespace app\index\controller;
+
+use think\Controller;
+use think\Request;
+
+class BaseController extends Controller {
+
+    public function __construct(Request $request = null)
+    {
+        parent::__construct($request);
+    }
+
+}
---
+++ application/index/controller/User.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/7
+ * Time: 15:05
+ * Comment: 用户控制器
+ */
+
+namespace app\index\controller;
+
+class User extends BaseController {
+
+}
